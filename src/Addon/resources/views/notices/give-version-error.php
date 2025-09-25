05269a8eac42a11eb4d7ffe709cc4ca7c1cc48c7
Jon Waldstein <jonwaldstein@mac.mynetworksettings.com>
2025-09-25 15:46:59 -0400

    fix: use correct verion number in notice

--- src/Addon/resources/views/notices/give-version-error.php
+++ src/Addon/resources/views/notices/give-version-error.php
@@ -4,6 +4,6 @@
 	<?php _e( 'Activation Error:', 'give-paystack' ); ?>
 </strong>
 <?php _e( 'You must have', 'give-paystack' ); ?> <a href="https://givewp.com" target="_blank">Give</a>
-<?php _e( 'version', 'give-paystack' ); ?> <?php echo GIVE_VERSION; ?>+
+<?php _e( 'version', 'give-paystack' ); ?> <?php echo GIVE_PAYSTACK_MIN_GIVE_VERSION; ?>+
 <?php printf( esc_html__( 'for the %1$s add-on to activate', 'give-paystack' ), GIVE_PAYSTACK_NAME ); ?>.
 
